5c55efd817047f627a138874ed08ffa5cd56b8b6
burkina100 <33917688+burkina100@users.noreply.github.com>
2018-02-24 14:15:06 +0800

    Delete changeBook.js

--- js/changeBook.js
+++
@@ -1,8 +0,0 @@
-var changeBook = function(element) {
-		var name = element.getAttribute("name");
-		var nameCollect = name.substr(0, name.length-1);
-		var bookImage = document.getElementById(nameCollect);
-		var num = bookImage.style.backgroundImage.lastIndexOf(nameCollect);
-		var str = bookImage.style.backgroundImage.substr(0, num);
-		bookImage.style.backgroundImage = str + name + '.png")';
-};
